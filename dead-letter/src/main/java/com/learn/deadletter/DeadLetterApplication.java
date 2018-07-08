b3c18a5065e8d4aebb7778588a663b33d4a5071c
callor-t420 <bingc420@qq.com>
2018-07-08 14:05:09 +0800

    初建项目

---
+++ dead-letter/src/main/java/com/learn/deadletter/DeadLetterApplication.java
@@ -0,0 +1,12 @@
+package com.learn.deadletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadLetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadLetterApplication.class, args);
+	}
+}
